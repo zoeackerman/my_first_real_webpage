33c5a98fc9c69c5285ad00dd4cbaba9dfa785ad2
zoeackerman <zoe@ztoasafetyservices.com>
2016-07-30 13:46:59 +1000

    Add "about" page
    Read about contents in "layout" template

--- build.rb
+++ build.rb
@@ -2,7 +2,6 @@ require 'erb'
 
 weekday = Time.now.strftime('%A')
 simple_template = File.read("src/layout.html.erb")
-puts simple_template
 
 renderer = ERB.new(simple_template)
 puts output = renderer.result()
